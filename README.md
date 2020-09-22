f2bd993b0b20bb2821edd5fbead9004ca5c17f29
akshay-1612 <akshayk.cs17@sahyadri.edu.in>
2020-09-22 12:53:39 +0530

    Pogram file added

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # TwitterBot
+Twitter Bot which automatically likes and retweet the tweets which has the hashtag #python,#AI,#ML,#Data Science    
+
+### Requirements
+Apply for ****Twitter developer account*** to get your API Keys
---
+++ main.py
@@ -0,0 +1,39 @@
+import tweepy
+import time
+
+
+api_key = '<api_key>'
+api_key_secreat = '<api_key_secreat>'
+access_token = '<access_token>'
+access_token_secreat ='<access_token_secreat>'
+
+auth = tweepy.OAuthHandler(api_key,api_key_secreat)
+auth.set_access_token(access_token,access_token_secreat)
+
+api = tweepy.API(auth,wait_on_rate_limit=True,wait_on_rate_limit_notify=True)
+
+user = api.me()
+
+search =["#python","#ML","#Data Science","#AI"]   # search tag
+numTweets = 500   #rate limit
+
+
+for tweet in tweepy.Cursor(api.search,search,lang="en").items(numTweets):
+    try:
+        print('Tweet liked')
+        tweet.favorite()
+
+        print('tweet retweeted')
+        tweet.retweet()
+
+        time.sleep(60*10) #sleep for 10 minutes
+        
+    except tweepy.TweepError as e:
+        print(e.reason)
+    except StopAsyncIteration:
+        break
+
+
+
+
+
